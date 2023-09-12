b6444129aa4e3ebd89e6394e170dadee1184e026
ongraphpythondev <pythondev@ongraph.com>
2023-09-12 11:31:40 +0530

    update to new version

--- convert_table.py
+++ convert_table.py
@@ -3,6 +3,9 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from dotenv import load_dotenv
 from langchain.agents import create_csv_agent
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 import argparse
 import pandas as pd
 import os
@@ -11,45 +14,35 @@ load_dotenv()
 OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
 
 def convert_table(source_csv, template_csv, target_csv):
-    
+
+    df1=pd.read_csv(source_csv).to_string()
+    df2=pd.read_csv(template_csv).to_string()
+    print(type(df1),type(df2))
+    print(df1)
+    template='''Your task is to take this Dataframe2 as a template dataframe and transform dataframe1 values in dataframe2 template, Do it in this manner that it cover every row of dataframe1. 
+
+                     Output Instructions: 
+                     1. output Should be the csv data. 
+                     2. output should not contain text data.
+                     3. output should not contain code.
+
+
+                     Dataframe1: {dataframe1}
+                     Dataframe2: {dataframe2}
+                     '''
+    # print(df2)
+    prompt_template=PromptTemplate(input_variables=["dataframe1","dataframe2"],template=template)
+    chain = LLMChain(llm=ChatOpenAI(openai_api_key=OPENAI_API_KEY,model='gpt-3.5-turbo'), prompt=prompt_template,verbose=True)
+    output=chain.run(dataframe1=df1,dataframe2=df2)
+    print(output)    
+
+
+    df = pd.read_csv(StringIO(output))
+
+    df.to_csv(target_csv,index=False)
 
 
 
-
-
-    agent = create_csv_agent(
-        ChatOpenAI(openai_api_key=OPENAI_API_KEY,temperature=0, model="gpt-4"),
-        [source_csv,template_csv],
-        verbose=True,
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
-
-    )
-    print(agent)
-    output=agent.run('''Your task is to mapping the df1 to the df2  by transferring values and transforming values into the format of the df2, do this on all rows of df1. 
-Output example:           
-    Date    EmployeeName         Plan PolicyNumber  Premium
-0  01-05-2023        John Doe    Gold Plan      AB12345    150.0
-1  02-05-2023      Jane Smith  Silver Plan      CD67890    100.0
-2  03-05-2023   Michael Brown  Bronze Plan      EF10111     50.0
-3  04-05-2023   Alice Johnson    Gold Plan      GH12121    150.0
-4  05-05-2023      Bob Wilson  Silver Plan      IJ13131    100.0
-5  06-05-2023  Carol Martinez  Bronze Plan      KL14141     50.0
-6  07-05-2023  David Anderson    Gold Plan      MN15151    150.0
-7  08-05-2023      Eva Thomas  Silver Plan      OP16161    100.0
-8  09-05-2023   Frank Jackson  Bronze Plan      QR17171     50.0
-9  10-05-2023     Grace White    Gold Plan      ST18181    150.0
-
-Instruction for output:
-                     1. it only contain the transformed dataframe of df1.
-                     2. it doen't anything than the transformed dataframe of df1.
-                     3. the output structure should match the example.
-                     4. don't return the code
-
-
-''')
-    print(output)
-    df = pd.read_csv(StringIO(output), delim_whitespace=True, skipinitialspace=True)
-    df.to_csv(target_csv)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert a source CSV using a template CSV")
     parser.add_argument("--source", required=True, help="Path to the source CSV file")
